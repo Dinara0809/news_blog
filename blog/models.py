f3a71a4763056aad7f1cf361cd4083ceadd8ff1a
Dinara <dinara080999@gmail.com>
2021-09-23 20:47:56 +0600

    hallo

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,19 @@
 from django.db import models
+from django.urls import reverse
 
 
 class News(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
+    photo = models.ImageField(upload_to='photos')
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+
+    def get_absolute_url(self):
+        return reverse('views_new', kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('logout/', user_logout, name='logout'),
     path('', HomeNews.as_view(), name='home'),
     path('category/<int:category_id>/', NewsByCategory.as_view(), name='category'),
-    path('news/<int:pk>/', ViewNews.as_view(), name='view_news'),
+    path('news/<int:pk>/', ViewNews.as_view(), name='views_new'),
     path('news/add-news/', CreateNews.as_view(), name='add_news')
 ]
